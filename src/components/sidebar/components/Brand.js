819fd80f1aa777507dc59b787bca68a1a72281cf
amoled27 <amodshinde1995@gmail.com>
2023-05-01 07:50:32 +0530

    feat: removed horizon branding

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -6,15 +6,21 @@ import { Flex, useColorModeValue } from "@chakra-ui/react";
 // Custom components
 import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
+import MobSFLogo from "assets/img/mobsf_icon.png";
+import { VSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
   //   Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
 
   return (
-    <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-      <HSeparator mb='20px' />
+    <Flex align="center" direction="column" style={{ background: "#000" }}>
+      {/* <Flex direction="row" align="center">
+        <img src={MobSFLogo} />
+        <VSeparator />
+        MobSF
+      </Flex>
+      <HSeparator mb="20px" /> */}
     </Flex>
   );
 }
